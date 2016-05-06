494089fdb0faff185ba4a7f196358e8a9bd930f4
sergey.choporoff <s.choporoff@gmail.com>
2016-05-06 16:51:20 +0300

    ООП

--- EDEBO-dict-loader/Program.cs
+++ EDEBO-dict-loader/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EDEBO_dict_loader.ua.edu.znu.edbo;
 
 namespace EDEBO_dict_loader
@@ -11,32 +7,20 @@ namespace EDEBO_dict_loader
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Guides...");
-            EDBOGuides guides = new EDBOGuides();
-            System.Console.WriteLine("Done.");
-            System.Console.Write("Login...");
-            string session = guides.Login("shevchenko.ruslana@edbo.gov.ua", "choporov007", 0, "Y0NzMXVGYnplb2lYZzhxVlA3ZUZ4eFJualhlNnowbkh2dmpTQ0FSNkc5U09iOW9yWExQUnVLZ1FWZVNIQmY5b2JMQ1ZaSHRvcmg5eFFka2pKWGlabUZvVnBFN3hTakZCYUROQkhEQ3FzQUFtTFQ5UzRKOE82a2NGeFJGdUs1rMC=");
-            if (session.Length != 36)
-            {
-                System.Console.WriteLine(session);
-                System.Console.ReadKey();
-                return;
-            }
             try
             {
-                Guid guid = new Guid(session);
+                EdboGuidesProcessor guides = new EdboGuidesProcessor();
+                //guides.Languages();
+                //guides.Universities();
+                //guides.UniversityFacultets();
+                guides.Specialities(1);
             }
-            catch(Exception exception)
+            catch (Exception ex)
             {
-                System.Console.WriteLine(exception.Message);
+                Console.WriteLine(ex.Message);
             }
-            
-            System.Console.WriteLine("Done.");
-            System.Console.Write("Logout...");
-            guides.Logout(session);
-            System.Console.WriteLine("Done.");
-            System.Console.WriteLine("Press any key");
-            System.Console.ReadKey();
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
         }
     }
 }
